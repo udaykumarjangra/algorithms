380f5393b2b6b85d6956fdb5f0b265717e28bc00
Ritika Dhiman <35415933+ritika99@users.noreply.github.com>
2020-10-30 16:47:43 +0530

    added calculator.py

---
+++ code/calculator.py
@@ -0,0 +1,24 @@
+#addition
+def add(x, y):
+    return x + y
+
+#subtract
+def subtract(x, y):
+    return x - y
+
+#multiplies
+def multiply(x, y):
+    return x * y
+
+#divides
+def divide(x, y):
+    return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter operation(1/2/3/4): ")
